f67711ac65370444794a36a5717c6767d77ba9db
manhngtien <nguyenmanh.4949@gmail.com>
2026-03-26 15:42:38 +0700

    🔨 setup jest & add script to calculate code coverage

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+
+  testMatch: [
+    "**/?(*.)+(spec|test).(ts|tsx)"
+  ],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|immer)/)'
+  ],
+
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
 };
